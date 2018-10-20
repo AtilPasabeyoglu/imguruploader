cbf980f33fd3aa922bc94101e8c8c7a6ce27da1d
Atıl Paşabeyoğlu <atilpasha@gmail.com>
2018-10-20 14:51:44 +0000

    Initial Commit

---
+++ helpers.go
@@ -0,0 +1,6 @@
+package main
+
+// Defining imgur tokens.
+// PlEASE CHANGE THESE BEFORE CREATING A TASK!!
+var clientSecret = "19bd8dcd055229d5b2943b9373011baa12a111bc"
+var clientID = "26e44081aea226f"
